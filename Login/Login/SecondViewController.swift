2e9dd0110449545acb65fce9cf2f1effb9ff0af3
ikurlovich <ikurlovich@icloud.com>
2023-11-04 20:44:33 +0400

    2. The first presentation is completed stage 1

---
+++ Login/Login/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  Login
+//
+//  Created by Илья Курлович on 04.11.2023.
+//
+
+import UIKit
+
+final class SecondViewController: UIViewController {
+    
+}
---
+++ Login/Login/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Илья Курлович on 04.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: IBOutlets -
+    
+    // MARK: Properties -
+    
+    // MARK: Life cycle -
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
